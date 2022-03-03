5aed798e0956f5a0ec1b44185658fc164ca24f0e
nane <nane.ohanjanyan@mail.ru>
2022-03-03 18:55:59 +0400

    4 code

---
+++ cpp/day1/4_homework_c.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+ int chickens, cows, pigs;
+ int sum = 0;
+  cout << " chickens = ";
+  cin >> chickens;
+  cout << " cows = ";
+  cin >> cows;
+  cout << " pigs = ";
+  cin >> pigs;
+ 
+sum = 2 * chickens + 4 * cows + 4 * pigs;
+cout << " Resulat = " << sum << "\n";
+   
+  
+  
+
+    return 0;
+}
+
