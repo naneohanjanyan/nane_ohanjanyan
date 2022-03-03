70d4a3e9e0b019bcbf4ab94e865a529c3530b7ab
nane <nane.ohanjanyan@mail.ru>
2022-03-03 18:23:39 +0400

    1 code

---
+++ cpp/day1/1_homework_c.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int a, b;
+cout << " Remember number:";
+cin >> a;
+while (a <=0 || a>=100){
+ cout << "\n Remember number: ";
+ cin >> a;
+}
+do{
+  cout << "\n Number = ";
+  cin >> b;
+  if(a < b) cout << " a < " << b;
+  else if (a > b) cout << " a > " << b;
+}while (a != b);
+
+
+
+return 0;
+}
+
+
