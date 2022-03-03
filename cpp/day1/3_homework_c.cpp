617f96e2921471e6f0168293b4790a3d0864d43d
nane <nane.ohanjanyan@mail.ru>
2022-03-03 18:45:46 +0400

    3 code

---
+++ cpp/day1/3_homework_c.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+int year = 2022;
+ while (year <= 2042){
+  if (year % 4 == 0){
+  cout << year << " leap year \n";
+  }
+ year ++;
+ }
+   
+  
+  
+
+    return 0;
+}
+
