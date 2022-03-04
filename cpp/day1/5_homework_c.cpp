28026325bceab148adace38d5ebad1b766addfd5
nane <nane.ohanjanyan@mail.ru>
2022-03-04 19:06:42 +0400

    5code

--- cpp/day1/5_homework_c.cpp
+++ cpp/day1/5_homework_c.cpp
@@ -4,17 +4,26 @@ using namespace std;
 
 int main()
 {
-int a, b;
-int perimeter = 0, area = 0;
-cout << " length = ";
-cin >> a;
-cout << " width = ";
-cin >> b;
 
-perimeter = a + b;
-cout << " perimeter = " << perimeter;
-area = a * b;
-cout << " area = " << area << "\n";
+int a, b;
+
+int perimeter = 0, area = 0;
+
+   cout << " length = ";
+
+   cin >> a;
+
+   cout << " width = ";
+
+   cin >> b;
+
+ perimeter = (2 * a) + (2 * b);
+
+ cout << " perimeter = " << perimeter;
+
+ area = a * b;
+
+ cout << " area = " << area << "\n";
   
   
 
