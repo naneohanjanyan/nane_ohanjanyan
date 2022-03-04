10ce728f81c83870ce32ff360406f90259a95296
nane <nane.ohanjanyan@mail.ru>
2022-03-04 19:05:54 +0400

    2code

--- cpp/day1/2_homework_c.cpp
+++ cpp/day1/2_homework_c.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
-#include <math.h>
+
 using namespace std;
 
 int main()
 {
+
 int a, b = 0, prod = 1;
+
 cout << " Number: ";
+
 cin >> a;
-while (b <= 10){
- prod = a * b;
- cout << a << " * " << b << " = " << prod << "\n";
- b++;
+
+while (b <= 10) {
+
+       	prod = a * b;
+ 
+	cout << a << " * " << b << " = " << prod << "\n";
+
+       	b++;
 
 }
    
