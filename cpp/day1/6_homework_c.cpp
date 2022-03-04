dd1f6ba847272fbebc58426aacb7d1df41542d1b
nane <nane.ohanjanyan@mail.ru>
2022-03-04 19:06:52 +0400

    6code

--- cpp/day1/6_homework_c.cpp
+++ cpp/day1/6_homework_c.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
-#include <math.h>
+
 using namespace std;
 
 int main()
 {
+
 int number, faktoryal = 1;
-cout << " Number = ";
-cin >> number;
+  
+   cout << " Number = ";
+
+   cin >> number;
+
 while (number > 0){
-faktoryal *= number;
-number -- ;
+  
+    faktoryal *= number;
+
+    number -- ;
+
 }
-cout << " Faktoryal = " << faktoryal << "\n";
+
+  :cout << " Faktoryal = " << faktoryal << "\n";
 
 
     return 0;
