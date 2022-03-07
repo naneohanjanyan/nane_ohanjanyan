29adfe9efe3932a0a8d1875398cba4dc2c63efe0
nane <nane.ohanjanyan@mail.ru>
2022-03-07 20:27:11 +0400

    10code

---
+++ cpp/day3/10_calculate_number.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+
+  double f ( double number1, double number2, char operat) {
+		  
+  double result;
+   
+    switch (operat) {
+         case '+':
+                result = number1 + number2;
+
+                 cout << number1 << " + " << number2 << " = " << result << "\n";
+
+                 break;
+
+        case '-':
+                 result = number1 - number2;
+
+                 cout << number1 << " - " << number2 << " = " << result << "\n";
+
+                 break;
+
+        case '*':
+		 result = number1 * number2;
+
+                 cout << number1 << " * " << number2 << " = " << result << "\n";
+
+                 break;
+
+        case '/':
+          
+	      	 if (number2 == 0){
+		  
+		     cout << " Division by 0 is not possible \n";
+	       
+		     break;
+		    
+	     } else {
+
+   	     	result = number1 / number2;
+
+                cout << number1 << " / " << number2 << " = " << result << "\n";
+	     
+                 break;
+
+	    }
+
+        default:
+                cout << " It's not an operator \n";
+
+ }
+
+
+     return 0;
+}
+
+int main () {
+
+   return f (10, 4, '*');
+
+}   
+
+
+
