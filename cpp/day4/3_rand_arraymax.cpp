4ebf62db4e4eb9a313378352795017e4a7286bdf
nane <nane.ohanjanyan@mail.ru>
2022-03-10 23:01:19 +0400

    3 code

--- cpp/day4/3_rand_arraymax.cpp
+++ cpp/day4/3_rand_arraymax.cpp
@@ -4,28 +4,11 @@
 
 using namespace std;
 
-int main () {
+void max1 ( double arr[], int size){
+    
+    double max = arr[0];
 
-srand(time(0));
-
-  int n;
-
-  cout << " Input size of the array: ";
-  
-  cin >> n;
-
-  int arr [n], max;
-
-  
-  for (int i = 0; i < n; i++){
-   
-	  arr[i] = rand () % 100;
-  	  
-  	  cout << arr[i] << "   ";	  
-  }
-	  max = arr[0];
-
-	  for (int i = 0; i < n - 1; i++){
+	  for (int i = 0; i < size - 1; i++){
 	  
 	  	if ( max < arr[i+1]){
 			
@@ -35,10 +18,35 @@ srand(time(0));
   
 	  }
 
-	  cout << "\n Max = " << max << endl;
+    cout << "\n Max = " << max << endl;
+}
+
+
+int main () {
+
+srand(time(0));
+
+  int n;
+
+  cout << " Input size of the array: ";
+  
+  cin >> n;
+  
+  const int size = n;
+
+  double arr [size], max;
+
+  for (int i = 0; i < size; i++){
+   
+	  arr[i] = rand () % 100 / 5.0;
+  	  
+  	  cout << arr[i] << "   ";	  
+  }
+	  
+   max1 (arr, size);
+
+	 
 
 return 0;
 
 }
-
-
