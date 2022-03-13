d6868378a89520468b3964e4909f60fbd7c4ad82
nane <nane.ohanjanyan@mail.ru>
2022-03-13 19:44:44 +0400

    change name of sum

--- cpp/day5/1_matrica.cpp
+++ cpp/day5/1_matrica.cpp
@@ -9,7 +9,7 @@ int main()
     
    const int m = 3;
    
-   int arr[m][m], sums[8];
+   int arr[m][m];
    
    int sum4 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum5 = 0, sum6 = 0, sum7 = 0, sum8 = 0;
    
@@ -24,7 +24,7 @@ int main()
         
          cout << endl;
          
-   }
+    }
         
         
     for(int i = 0; i < m; i++){
@@ -34,59 +34,56 @@ int main()
             if (i + j < m -1){ 
                    
                 sum1 += arr[i][j];
-                sums[0]= sum1;
             }
 
             if (i+ j > m -1){ 
                    
                 sum2 += arr[i][j]; 
-                sums[1] = sum2;
             }
 
             if ( i > j){ 
                
                 sum3 += arr[i][j]; 
-                sums[2] = sum3;
             }
                 
             if(i < j){
                 
                 sum4 += arr[i][j]; 
-                sums[3] = sum4;
             }
                 
             if(i < j && i + j < m - 1){
                    
                 sum5 += arr[i][j];
-                sums[4]= sum5;
             }
                
             if(i > j && i + j < m - 1){
               
                 sum6 += arr[i][j];
-                sums[5] = sum6;
             } 
             
             if(i > j && i + j > m - 1){
                   
                 sum7 += arr[i][j];
-                sums[6] = sum5;
             } 
                 
             if(i < j && i + j > m - 1){
              
                 sum8 += arr[i][j];
-                sums[7] = sum8;
             } 
         }
     }
 
-    for(int i = 0; i < 8; i++){
-        
-        cout << " sum " << i + 1 << " = "  << sums[i]  <<  ";  ";
-    }
-    
-    cout << endl;
+
+    cout << " above the secondary diagonal = " << sum1 << endl;
+    cout << " below the secondary diagonal = " << sum2 << endl;
+    cout << " below the main diagonal = " << sum3 << endl;
+    cout << " above the main diagonal = " << sum4 << endl;
+    cout << " small triangle above = " << sum5 << endl;
+    cout << " small triangle on the left = " << sum6 << endl;
+    cout << " small triangle inside = " << sum7 << endl;
+    cout << " small right triangle = " << sum8 << endl;
     
  return 0;
 }
+
+ 
