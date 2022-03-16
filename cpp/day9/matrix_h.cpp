fe74908eaa09e050740893c6609de457c2df4754
nane <nane.ohanjanyan@mail.ru>
2022-03-16 23:50:12 +0400

    add function

---
+++ cpp/day9/matrix_h.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "matrix.h"
+
+void sort(int **arr, int row, int column){
+	int temp;
+	for(int **ptr  = arr; ptr <= arr + row - 1; ptr++) {
+		for(int *ptr1 = *ptr; ptr1 <= *ptr + column - 1; ptr1++) {
+			for(int *ptr2 = ptr1; ptr2 <= *ptr + column - 1; ptr2++) {
+				if(*ptr2 < *ptr1) {
+
+					temp = *ptr2;
+					*ptr2 = *ptr1;
+					*ptr1 = temp;
+				}
+			}
+		}
+	}
+}
+
+
