7523cb502454d7b33ca5c0487853ccb0f0d505ef
naneohanjanyan <100269250+naneohanjanyan@users.noreply.github.com>
2022-03-24 22:20:41 +0400

    Delete cpp directory

--- cpp/lib/matrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef SORT 
-#define SORT
-
-void sort(int **, int, int);
-
-void sort_column(int **, int, int);
-  
-#endif
-
--- cpp/lib/my_atoi.h
+++
@@ -1,6 +0,0 @@
-#ifndef MY_ATOI
-#define MY_ATOI
-
- int my_atoi(char *);
-
-#endif
