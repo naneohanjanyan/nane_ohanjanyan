243d3b9000dcdb76bfc69480e44a6f7f9bf66756
nane <nane.ohanjanyan@mail.ru>
2022-03-29 21:26:05 +0400

    added city game

--- cpp/day10/2_city_game.cpp
+++ cpp/day10/2_city_game.cpp
@@ -23,27 +23,27 @@ return 0;
 }
 
 void game(int choose) {
-	string city[] = {"austin", "boston", "chicago", "columbia", "dayton", "denver", "glendale", "hollywood", "los angeles", "miami", "new orleans", "new york", "odessa", "philadelphia", "erevan", "moscow", "prague", "paris", "berlin", "ankara", "baku", "ekaterinburg","grozni", "irkutsk", "ninve", "valencia", "astana", "dublin", "zaltsburg", "pekin", "mumbai"};
- 	int size = sizeof(city)/sizeof(city[0]);
+	string city[] = {"washington", "austin", "boston", "chicago", "columbia", "dayton", "denver", "glendale", "hollywood", "los angeles", "miami", "new orleans", "new york", "odessa", "philadelphia", "erevan", "moscow", "prague", "paris", "berlin", "ankara", "baku", "ekaterinburg","grozni", "irkutsk", "ninve", "valencia", "astana", "dublin", "zaltsburg", "pekin", "mumbai"};
+ 	int size = sizeof(city) / sizeof(city[0]);
 	int random_index = rand() % size;
 	string user;
 	bool check = false, win_break = false; 
 
 	while (true) {
 		if (choose == 2) {
- 			cout << endl;
 			cout << " computer: \t" << city[random_index];
 			choose = 1;
 			check = true;
+ 			cout << endl;
 		}
 		if (choose == 1) {
-			cout <<  endl;
 			cout << " user:   \t";
 			cin >> user;
 			choose = 2;
+			cout <<  endl;
 		}
 		if (check && city[random_index][city[random_index].length() - 1] != user[0]) {
-			cout << "  You Lose \n";
+			cout << " You Lose \n";
 			break;
 		}
 		if (check) {
@@ -55,7 +55,7 @@ void game(int choose) {
 				break;
 			}
 			if (i == (size - 1)) {
-				cout << " You Win " << endl;
+				cout << " You Win \n";
 				win_break = true;
 				break;
 			}
