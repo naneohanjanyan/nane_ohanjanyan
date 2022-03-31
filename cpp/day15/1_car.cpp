2b6eb1c77d1644745604f32f9cb53c9119917ba6
nane <nane.ohanjanyan@mail.ru>
2022-03-31 18:28:33 +0400

    change code

--- cpp/day15/1_car.cpp
+++ cpp/day15/1_car.cpp
@@ -2,23 +2,34 @@
 #include <fstream>
 #include <cstring>
 using namespace std;
+
 struct information {
 	string brand;
 	string model;
 	int price;
 	int mileage;
-	double engine_power;
+	double power;
 	string transmission;
 	string color;
 };
 
 void print (information info);
+
 int main () {
 
-struct information info[10];
+fstream file1;
+file1.open ("car_info.txt");
+string str1 = "";
+int row = 0;
+while (getline(file1,str1)) {
+	 row++;
+}
+file1.close();
+
+struct information info[row];
 ifstream file;
 file.open("car_info.txt");
-string str = "", info_string[10];
+string str = "", info_string[row];
 int k = 0, j = 0;
 // add file in struct
 while(!file.eof()){
@@ -29,7 +40,7 @@ while(!file.eof()){
 		info[j].model = info_string[1].substr(0, info_string[1].size() - 1);
 		info[j].price = stoi(info_string[2].substr(0, info_string[2].size()-1));
 		info[j].mileage =stoi( info_string[3].substr(0, info_string[3].size()-1));
-		info[j].engine_power = stod(info_string[4].substr(0, info_string[4].size()-1)); 			
+		info[j].power = stod(info_string[4].substr(0, info_string[4].size()-1)); 			
 		info[j].transmission = info_string[5].substr(0, info_string[5].size()-1);
 		info[j].color = info_string[6];
 		j++;
@@ -38,85 +49,98 @@ while(!file.eof()){
 	k++;
 }
 file.close();
+cout << "Search modes: 0 - show, 1 - brand, 2 - model, 3 - min-price, 4 - max-price, 5 - min-mileage, 6 - max-mileage, 7 - transmission, 8 - color, 9 - engine power\n";
+bool condition = true;
+bool    brand_ = false, 
+	model_ = false, 
+	min_price_ = false,
+	max_price_ = false, 
+	min_mileage_ = false, 
+	max_mileage_ = false, 
+	transm_ = false,
+	color_ = false,
+	power_ = false; 
 
-cout << " Search modes: brand, min-price, max-price, transmission, color \n";
-cout << " Input search mode 1:  "; 
-string mode1 = "", mode2 = "";
-cin >> mode1;
-cout << " Input search mode 2:  ";
-cin >> mode2;
-string brand, color, transm;
-int min_price, max_price;
+string brand, model,  color, transm;
+int min_price, max_price, min_mileage, max_mileage;
+double power;
 
-if( mode1 == "brand" || mode2 == "brand"){
-	cout << " Input data of brand: ";
-	cin >> brand;
-}
-if ( mode1 == "color" || mode2 == "color" ) {
-	cout << " Input data of color: ";
-	cin >> color;
-}
-if( mode1 == "transmission" || mode2 == "transmission"){
-	cout << " Input data of transmission: ";
-	cin >> transm;
-}
-if ( (mode1 == "min-price" && mode2 == "max-price") || (mode2 == "min-price" && mode1 == "max-price")){
-	cout << " Inpute min price: "; 
-	cin >> min_price;
-	cout << " Input max price: ";
-	cin >> max_price;
-} else if( mode1 == "min-price" || mode2 == "min-price" ) {
-	cout << " Inpute min price: "; 
-	cin >> min_price;
-} else if(mode1 ==  "max-price" || mode2 == "max-price" ) {
-	cout << " Input max price: ";
-	cin >> max_price;
-} else {
-	cout << " Is not search mode \n";
-}
-//search cars from filter
+while(condition) {
+	int mode;
+	cout << " Input search mode: ";
+	cin >> mode;
+	cin.ignore();
+	switch (mode) {
+		case 0:
+		condition = false;
+		break;
 
-if( ((mode1 == "brand" && mode2 == "min-price") || (mode1 == "min-price" && mode2 == "brand")) || ((mode1 == "brand" && mode2 == "max-price") || ( mode1 == "max-price" && mode2 == "brand") )) {
-	for (int i = 0; i < 10; i++) {
-		if (info[i].brand == brand && info[i].price >= min_price) {
-			print(info[i]);
-		} else if (info[i].price < max_price && info[i].brand == brand) {
-			print (info[i]);
-		}
+		case 1:
+		cout << " Brand: ";
+		getline(cin, brand);
+		brand_ = true;
+		break;
+
+		case 2:
+		cout << " Model: ";
+		getline(cin, model);
+		model_ = true;
+
+		case 3:
+		cout << " Min price: ";
+		cin >> min_price;
+		min_price_ = true;
+		break;
+
+		case 4:
+		cout << " Max price: ";
+		cin >> max_price;
+		max_price_ = true;
+		break;
+
+		case 5:
+		cout << " Min mileage: "; 		
+		cin >> min_mileage;
+		min_mileage_ = true;
+		break;
+
+		case 6:
+		cout << " Max mileage: ";
+		cin >> max_mileage;
+		max_mileage_ = true;
+		break;
+
+		case 7:
+		cout << " Transmission: ";
+		cin >> transm;
+		transm_ = true;
+		break;
+
+		case 8:
+		cout << " Color: ";
+		cin >> color;
+		color_ = true;
+		break;
+
+		case 9:
+		cout << " Engine power: ";
+		cin >> power; 
+		power_ = true;
+		break;
 	}
-} else if ( ( ( mode1 == "transmission" && mode2 == "brand") || (mode1 == "brand" && mode2 == "transmission")) || (( mode1 == "brand" && mode2 == "color") || ( mode1 == "color" && mode2 == "brand")) ) {
-	for (int i = 0; i < 10; i++) {
-		if (info[i].transmission == transm && info[i].brand == brand) {
-			print(info[i]);
-		}
-		if(info[i].color == color && info[i].brand == brand) {
-			print(info[i]);
-		}
-	}
-} else if ( ((mode1 == "min-price" && mode2 == "max-price" ) || (mode1 == "max-price" && mode2 == "min-price")) || ( (mode1 == "min-price" && mode2 == "transmission") ||( mode2 == "min-price" && mode1 == "transmission")) || ((mode1 == "min-price" && mode2 == "color") || (mode1 == "color" && mode2 == "min-price")) ) {
-	for (int i = 0; i < 10; i++) {
-		if (info[i].price >= min_price && info[i].price <= max_price) {
-			print (info[i]);
-		} else if ( info[i].transmission == transm && info[i].price >= min_price ) {
-			print(info[i]);
-		} else if ( info[i].color == color && info[i].price >= min_price ) {
-			print(info[i]);
-		}
-	}
-} else if ( ((mode1 == "max-price" && mode2 == "color") || (mode1 == "color" && mode2 == "max-price")) || ((mode1 == "max-price" && mode2 == "transmission") || (mode2 == "max-price" && mode1 == "transmission")) ) {
-	for (int i = 0; i < 10; i++) {
-		if (info[i].color == color && info[i].price <= max_price) {
-			print(info[i]);
-		} else if (info[i].transmission == color && info[i].price <= max_price) {
-			print(info[i]);
-		}
-	}
-} else if ( (mode1 == "transmission" && mode2 == "color") || (mode1 == "color" && mode2 == "transmission") ) { 
-	for (int i = 0; i < 10; i++) {
-		if( info[i].color == color && info[i].transmission == transm) {
-			print(info[i]);
-		}
-	}
+}
+
+for ( int i = 0; i < row; i++) {
+	if (brand_ && brand != info[i].brand) continue;
+	if (model_ && model != info[i].model) continue;
+	if (min_price_ && min_price > info[i].price) continue;
+	if (max_price_ && max_price < info[i].price) continue;
+	if (min_mileage && min_mileage < info[i].mileage) continue;
+	if (max_mileage_ && max_mileage > info[i].mileage) continue;
+	if (transm_ && transm != info[i].transmission) continue;
+	if (color_ && color != info[i].color) continue;
+	if (power_ && power != info[i].power) continue;
+	print(info[i]);
 }
 
 return 0;
@@ -129,8 +153,7 @@ void print( information info){
 	cout << " Model:        " << info.model << endl;
 	cout << " Price:        " << info.price << " $\n";
 	cout << " Mileage:      " << info.mileage << " km\n";
-	cout << " Engine power: " << info.engine_power << " L\n";
+	cout << " Engine power: " << info.power << " L\n";
 	cout << " Transmission: " << info.transmission << endl;
 	cout << " Color:        " << info.color << endl;
-	cout << "================================================================\n";
 }
