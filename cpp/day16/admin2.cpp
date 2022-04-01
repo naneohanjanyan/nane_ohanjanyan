dfd212f5ebbca5ed7210c23c03dc5930c90d7921
nane ohajanyan <nane.ohajanyan@mail.ru>
2022-04-01 12:53:33 +0400

    code

--- cpp/day16/admin2.cpp
+++ cpp/day16/admin2.cpp
@@ -2,7 +2,10 @@
 #include <fstream>
 using namespace std;
 
+int indexCount();
+
 int main() {
+/*
 string country;
 string city;
 string address;
@@ -11,11 +14,14 @@ int star;
 int singlRoom;
 int twinRoom;
 int trplRoom;
+int price1;
+int price2;
+int price3;
 bool spa;
 bool fitnes;
-
+*/
 loop:
-int size = 10;
+int size = 13;
 string info[size];
 cout << " Country: ";
 getline(cin,info[0]);
@@ -29,39 +35,64 @@ cout << " Star: ";
 cin >> info[4];
 cout << " Singl room: ";
 cin >> info[5];
-cout << " Twin room: ";
+cout << "Price Singl room: ";
 cin >> info[6];
-cout << " Triple room: ";
+cout << " Twin room: ";
 cin >> info[7];
-cout << " Spa ( 1 or 0 ): ";
+cout << "Price Twin room: ";
+cin  >> info[8];
+cout << " Triple room: ";
 cin >> info[9];
-cout << " Fitnes ( 1 or 0 ): ";
-cin >> info[9]; 
-
+cout << "Price Triple room: ";
+cin  >> info[10];
+cout << " Spa ( yes or not ): ";
+cin >> info[11];
+cout << " Fitnes ( yes or not ): ";
+cin >> info[12];
+int index = indexCount() + 1; 
+cout << 0 << endl;
 fstream hotel;
-hotel.open("hotel2.txt");
+hotel.open("hotel2.txt", fstream::in | fstream::out | fstream::app );
 if(hotel.is_open()) {
+	hotel << index << ". ";
 	for (int i = 0; i < size; i++) {
 		if( i != size - 1) {
 			hotel << info[i] << ", ";
-		} else if ( i == size - 1 ) {
+		} 
+		if ( i == size - 1 ) {
 			hotel << info[i];
 		}
-		hotel << "\n";
 	}
 }
+hotel << "\n";
 hotel.close();
 
 
 string command;
-cout << "Exit  or Countinue";
+cout << "Exit  or Countinue: ";
 cin >> command;
 if(command == "Countinue") {
+	cin.ignore();
 	goto loop;
 }
 
 return 0;
 
+}
+
+
+int indexCount() {
+  int index = 0;
+ifstream f;
+ f.open("hotel2.txt");
+string str = "";
+  while( getline(f, str) ){
+    index++;
+    cout << 1 << " " ;
+  }
+f.close();
+
+  return index;
 }
 
 
