5d42da58ed7593dd7310dada8d7e16eb1344fa78
naneohanjanyan <100269250+naneohanjanyan@users.noreply.github.com>
2022-04-01 14:36:37 +0300

    Update 3_homework_c.cpp

--- cpp/day1/3_homework_c.cpp
+++ cpp/day1/3_homework_c.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
+int main() {
 
 int year = 2022;
-
 while (year <= 2042){
-
       	if (year % 4 == 0){
-  
-	cout << year << " leap year \n";
-        }
-        year ++;
- }
-   
-  
-  
-
-    return 0;
+		cout << year << " leap year \n";
+	}
+	year ++;
+}
+return 0;
 }
 
