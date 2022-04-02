8eae68a0a848bb60343f20212ea747fdaee7129b
nane <nane.ohanjanyan@mail.ru>
2022-04-02 17:08:21 +0400

    changes syntax

--- cpp/day08/1_my_atoi.cpp
+++ cpp/day08/1_my_atoi.cpp
@@ -3,13 +3,11 @@
 using namespace std;
 
 int main (int argc, char *argv[]){
+
 int sum = 0;
-
 if( argc == 3){
-
-sum = my_atoi(argv[1]) + my_atoi(argv[2]);
-
-cout << sum << endl;
+	sum = my_atoi(argv[1]) + my_atoi(argv[2]);
+	cout << sum << endl;
 }
 return 0;
 }
