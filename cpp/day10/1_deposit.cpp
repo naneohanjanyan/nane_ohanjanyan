e4b889ecad745e8497905da72a5da3cfe9cff21a
nane <nane.ohanjanyan@mail.ru>
2022-04-04 20:48:03 +0400

    correcting error

--- cpp/day10/1_deposit.cpp
+++ cpp/day10/1_deposit.cpp
@@ -4,57 +4,50 @@
 #include <cstring>
 using namespace std;
 
-float f(float mother_sum, float percent, int month) {
-
-	float month_percent = percent / 12, sum = mother_sum;
-	sum += (sum * month_percent) / 100;
-
-return sum;
+float month_sum(float mother_sum, float percent) {
+	return mother_sum * (percent / 100) / 12;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+time_t now = time(0);
+char* date_time = ctime(&now);
+string date[5];
+int x = 0;
+for(int i = 0; date_time[i] != '\0'; i++) {
+	if(date_time[i] == ' ' && date_time[i+1] != ' ') {
+		x++;
+		continue;
+	}
+	date[x] += date_time[i];
+}
+int day = stoi(date[2]);
+int year = stoi(date[4]);
+int m;
+string month_str[12] = {"Jan ", "Feb ", "Mar ", "Apr ", "May ", "Jun ", "Jul ", "Agu ", "Sep ", "Oct ", "Nov ", "Dec "};
+for(int j = 0; j < 12; j++) {
+	if(month_str[j] == date[1]) {
+		m = j;
+		break;
+	}
+}
 
 float mother_sum = atof(argv[1]);
 int month = atoi(argv[2]);
 float percent = atof(argv[3]);
-cout << " mother sum = " << mother_sum;
-cout  << ", percent = " << percent << ", month number = " << month << endl;
+float monthly;
+cout << " mother sum = " << mother_sum << " amd  "
+	<< ", percent = " << percent 
+	<< ", month number = " << month << endl;
 
-time_t now = time(0);
-char* date_time = ctime(&now);
-string str[5];
-int x = 0;
-for(int i = 0; date_time[i] != '\0'; i++) {
-	if(date_time[i] == ' ') {
-		x++;
-		continue;
+for( int i = 0; i < month;i++) {
+	monthly = month_sum(mother_sum, percent);
+	mother_sum += monthly;
+	cout << month_str[m] << ' ' << day << ' ' << year << " | " << mother_sum << " amd " << monthly << endl;
+	m++;
+	if(m == 12) {
+		year++;
+		m = 0;
 	}
-	str[x] += date_time[i];
-}
-
-string month_str[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Agu", "Sep", "Oct", "Nov", "Dec"};
-int year = stoi(str[4]);
-int y = 0;
-for(int i = 0; i < 11; i++) {
-	if(str[1] ==month_str[i]) {
-		y = i;
-		/*for(int j = 0; j < 20; j++) {
-			str[1] = month[(j + i + 1) % 12];
-			cout << str[1] << year <<  " ";
-			if(str[1] == "Dec") year++;
-		}
-	break;*/
-	}
-}
-
-//f(mother_sum, percent, month);
-for( int i = 0; i < month; i++) {
-	str[1] = month_str[(y + i + 1) % 12];
-	//cout << str[1] << year <<  " ";
-	if(str[1] == "Dec") {
-		 year++;
-	}
-cout << str[1] << ' ' << str[2]<< ' ' << year << "  " << f(mother_sum, percent, month) << " amd " << endl;     //fixed << setprecision(2) << sum; 
 }
 
 return 0;
