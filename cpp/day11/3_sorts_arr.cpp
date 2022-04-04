80905b8695d8e612a3f6ff5cd7cbc82a8be57807
nane <nane.ohanjanyan@mail.ru>
2022-04-04 21:16:10 +0400

    change

--- cpp/day11/3_sorts_arr.cpp
+++ cpp/day11/3_sorts_arr.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
-
 using namespace std;
+
 void sort(int arr[], int size) {
-    int count = 0, element = 0, max_len = 0, first_el = 0;
-    for(int i = 0; i < size; i++){
-        count = 0;
-        for( int j = i; j < size - 1; j++){
-            if(arr[j] > arr[j + 1]){
-                break;
-            } else {
-                count ++;
-            }
-        }
-        element = count;
-            if(element > max_len){
-                max_len = element;
-                first_el = i;
-            }
-    }
-   
-    for( int i = first_el; i <= first_el + max_len; i++){
-        cout << arr[i] << "  ";
-    }
-cout << endl;
+	int element = 0, max_len = 0, first_el = 0;
+	for(int i = 0; i < size; i++){
+        	int count = 0;
+        	for( int j = i; j < size - 1; j++){
+			if(arr[j] > arr[j + 1]){
+                		break;
+            		} else {
+                		count ++;
+            		}
+        	}
+        	element = count;
+            	if(element > max_len){
+                	max_len = element;
+                	first_el = i;
+            	}
+	}
+	cout << " The segment with the highest growth rate: ";
+	for( int i = first_el; i <= first_el + max_len; i++){
+        	cout << arr[i] << "  ";
+	}
+	cout << endl;
 }
-int main()
-{
-    int size;
-    cout << " Enter array size: ";
-    cin >> size;
-    int *arr = new int [size];
-    cout << " Enter array elements: \n";
-    for(int i = 0; i < size; i++) {
-        cout << "arr[" << i << "] = ";
-        cin >> arr[i];
-    }
+
+int main() {
+ 
+int size;
+cout << " Enter array size: ";
+cin >> size;
+int *arr = new int [size];
+cout << " Enter array elements: \n";
+for(int i = 0; i < size; i++) {
+	cout << "arr[" << i << "] = ";
+	cin >> arr[i];
+}
     
-    sort(arr, size);
+sort(arr, size);
     
-    return 0;
+return 0;
 }
