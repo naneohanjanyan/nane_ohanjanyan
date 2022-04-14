886af4cb407e9e7901e0ff0f16237bfc9f05a601
nane <nane.ohanjanyan@mail.ru>
2022-04-14 22:08:20 +0400

    added

--- oop/day02/polynomial.cpp
+++ oop/day02/polynomial.cpp
@@ -2,63 +2,97 @@
 #include "polynomial.h"
 
 using namespace std;
-void Polynomial::Add() {
-	if (_degree1 == _degree2 ) {
-		for (int i = 0; i <= _degree1; i++) {
-			result[i] = arr1[i] + arr2[i];
-			cout << result[i] << "x^" << i << " + ";
-		}
-	} else if ( _degree1 > _degree2) {
-		for(int i = 0; i <= _degree1; i++) {
-			if( i <= _degree2) {
-				result[i] = arr1[i] + arr2[i];
-			} else {
-					result[i] = arr1[i];
- 			}		
-			cout << result[i] << "x^" << i << " + ";
 
-		}
-	} else if(_degree2 > _degree1) {
-		for(int i = 0; i <= _degree1; i++) {
-			if( i <= _degree1) {
-				result[i] = arr1[i] + arr2[i];
-			} else {
-				result[i] = arr2[i];
- 			}
-			cout << result[i] << "x^" << i << " + ";
-		}
+Polynomial::Polynomila() {
+	_arrSize = 1;
+	_coeff = new int [1];
+	_coeff[0] = 0;
+}
+
+Polynomial::Polynomial(int degree) {
+	_arrSize = degree + 1;
+	_coeff = new int[_arrSize];
+	for(int i = 0; i < _arrSize; i++) {
+		_coeff[i] = 0;
 	}
 }
 
-void Polynomial ::Subtract() {
-	if (_degree1 == _degree2 ) {
-		for (int i = 0; i <= _degree1; i++) {
-			result[i] = arr1[i] - arr2[i];	
-			cout << result[i] << "x^" << i << " - ";
-
-		}
-	} else if ( _degree1 > _degree2) {
-		for(int i = 0; i <= _degree1; i++) {
-			if( i <= _degree2) {
-				result[i] = arr1[i] - arr2[i];
-			} else {
-					result[i] = arr1[i];
- 			}
-			cout << result[i] << "x^" << i << " - ";
-		}
-	} else if(_degree2 > _degree1) {
-		for(int i = 0; i <= _degree1; i++) {
-			if( i <= _degree1) {
-				result[i] = arr1[i] - arr2[i];
-			} else {
-				result[i] = arr2[i];
- 			}
-			cout << result[i] << "x^" << i << " - ";
-
-		}
+Polynomial::Polynomial(int *coefficientArr, int coefficientCount) {
+	_arrSize =  coefficientCount;
+	_coeff = new int [coefficientCount];
+	for(int i = 0; i < coefficientCount; i++) {
+		_coeff[i] = coefficientArr[i];
 	}
 }
+// copy constructor
+Polynomial::Polynomial(Polynomial &other) {
+	_arrSize = other._arrSize;
+	_coeff = new int [_arrSize];
+	for(int i = 0; i < _arrSize; i++) {
+		_coeff[i] = other._coeff;
+	}
+}
+// destructor
+Polynomial::~Polynomial() {
+	delete[] _coeff;
+	_arrSize = 0;
+	_coeff = nullptr;
+}
+ void Polynomial::Add(Polynomial &other) {
+	int sizeOfArr = (_arrSize >= other._arrSize) ? _arrSize : other._arrSize;
+	Polynomial answer(sizeOfArr - 1);
+	if(_arrSize >= other._arrSize) {
+		for(int i = 0; i < other._arrSize; i++) {
+			aswer._coeff[i] = _coeff[i] + other._coeff[i];
+		} 
+		for(int i = other._arrSize; i <_arrSize; i++) {
+			aswer._coeff[i] = _coeff[i];
+		} 
+	} else {
+		for(int i = 0; i < _arrSize; i++) {
+			aswer._coeff[i] = _coeff[i] + other._coeff[i];
+		} 
+		for(int i = _arrSize; i < other._arrSize; i++) {
+			aswer._coeff[i] = other._coeff[i];
+		} 
+	}
+	return answer;
+	
+}
 
+void Polynomial::Subtract(Polynomial &other) {
+	int sizeOfArr = (_arrSize >= other._arrSize) ? _arrSize : other._arrSize;
+	Polynomial answer(sizeOfArr - 1);
+	if(_arrSize >= other._arrSize) {
+		for(int i = 0; i < other._arrSize; i++) {
+			aswer._coeff[i] = _coeff[i] - other._coeff[i];
+		} 
+		for(int i = other._arrSize; i <_arrSize; i++) {
+			aswer._coeff[i] = _coeff[i];
+		} 
+	} else {
+		for(int i = 0; i < _arrSize; i++) {
+			aswer._coeff[i] = _coeff[i] - other._coeff[i];
+		} 
+		for(int i = _arrSize; i < other._arrSize; i++) {
+			aswer._coeff[i] = other._coeff[i];
+		} 
+	}
+	return answer;
+	
+}
+Polynomial:: Multiplay(Polynomial &other) {
+	int degreeOfAnswer = _arrSize + othet._arrSize - 2;
+	Polynomial answer (degreeOfAnswer);
+	for(int i = 0; i < _arrSize; i++) {
+		for(int j = 0; j < other._arrSize; j++) {
+			answer._arrSize[i+j] += (_coeff[i] * other._coeff[j]);
+		}
+	}
+	return 0;
+}
+
+/*
 void Polynomial::MultiplyByNumber() {
 	int number;
 	cin >> number; 
@@ -72,4 +106,4 @@ void Polynomial::MultiplyByNumber() {
 		cout << result[i] << "x^" << i << " + ";
 	}
 }
-
+*/
--- oop/day02/polynomial.h
+++ oop/day02/polynomial.h
@@ -4,34 +4,19 @@ using namespace std;
 
 class Polynomial {
 	private:
-		int _degree1, _degree2;
-		int *arr1 = new int[_degree1 + 1];
-		int *arr2 = new int[_degree2 + 1];
-		int *result;
+		int * _coeff;
+		int _arrSize;
 	public:
-		void MultiplyByNumber();
-		void Add();
-		void Subtract();
-		Polynomial(int degree1, int degree2, int Arr1[], int Arr2[]){
-			_degree1 = degree1;
-			_degree2 = degree2;
-			int result_arr_size;
-			if(degree1 < degree2) {
-				result_arr_size = degree2;
-			} else {
-				result_arr_size = degree1;
-			}
-			result = new int [result_arr_size];
-
-			for(int i = 0; i <= degree1; i++) {
-				*(arr1 + i) = Arr1[i];
-			}
-			for(int i = 0; i <= degree2; i++) {
-				*(arr2 + i) = Arr2[i];
-			}
-		}
-		~Polynomial() {
-		}
+		Polynomial();
+		Polynomial(int *coefficientArr, int coefficientCount);
+		Polynomial(Polyomial& other);
+		~Polynomial();
+		Polynomial MultiplyByNumber(Polyomial &other);
+		Polynomial Add(Polyomial &other);
+		Polynomial Subtract(Polyomial &other);
+		Polynomial Multiply(Polyomial &other);
+		int degree();
+		void print();
 };
 
 #endif
