ce07a7ce6b90681e7192f5585f2d5a23427bf51e
nane <nane.ohanjanyan@mail.ru>
2022-04-16 00:09:12 +0400

    operator overloading

---
+++ oop/day03/poly.h
@@ -0,0 +1,19 @@
+#ifndef POLYNOMIAL
+#define POLYNOMIAL
+
+class Polynomial {
+	private:
+		int * _coeff;
+		int _arrSize;
+	public:
+		Polynomial();
+		Polynomial(int degree);
+		Polynomial(int *coefficientArr, int coefficientCount);
+		Polynomial(Polynomial& other);
+		Polynomial operator+( const Polynomial &other ) const; 
+		Polynomial operator-( const Polynomial &other ) const; 
+		Polynomial operator*( const Polynomial &other ) const; 
+		int degree();
+		void print();
+};
+#endif
