93b8fa665ff0c9ebb458cf798e3bc75ee28edf73
nane <nane.ohanjanyan@mail.ru>
2022-05-05 17:36:06 +0400

    added

--- date_structure/day6/createTree.cpp
+++ date_structure/day6/createTree.cpp
@@ -6,80 +6,105 @@ using namespace std;
 
 bool checkRoot(bool **arr, int size);
 
-void inputMatrix(bool **arr, int size) {
-  for (int i = 0; i < size; i++) {
+void inputMatrix(bool **arr, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
     arr[i] = new bool[size];
-    for (int j = 0; j < size; j++) {
+    for (int j = 0; j < size; j++)
+    {
       cin >> arr[i][j];
     }
   }
 }
 
-bool checkTree(bool **arr, int size) {
-  for (int i = 0; i < size; i++) {
-    int oneCount = 0;
-    for (int j = 0; j < size; j++) {
-      if (arr[i][j] == 1 && arr[j][i] == 1) {
+bool checkTree(bool **arr, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    int numberOfUnitsInRow = 0;
+    for (int j = 0; j < size; j++)
+    {
+      if (arr[i][j] == 1 && arr[j][i] == 1)
+      {
         return false;
       }
-      if (arr[i][j] == 1) {
-        oneCount++;
+      if (arr[i][j] == 1)
+      {
+        numberOfUnitsInRow++;
       }
-      if (oneCount > 2) {
+      if (numberOfUnitsInRow > 2)
+      {
         return false;
       }
     }
   }
-  if (!checkRoot(arr, size)) {
+  if (!checkRoot(arr, size))
+  {
     return false;
-  } else {
+  }
+  else
+  {
     return true;
   }
 }
 
-bool checkRoot(bool **arr, int size) {
-  int rootCount = 0;
-  for (int i = 0; i < size; i++) {
-    int oneCountinColumn = 0;
+bool checkRoot(bool **arr, int size)
+{
+  int rootNumber = 0;
+  for (int i = 0; i < size; i++)
+  {
+    int numberOfUnitsInColumn = 0;
     int count = 0;
-    for (int j = 0; j < size; j++) {
-      if (arr[j][i] == 0) {
+    for (int j = 0; j < size; j++)
+    {
+      if (arr[j][i] == 0)
+      {
         count++;
       }
 
-      if (count == size) {
-        rootCount++;
+      if (count == size)
+      {
+        rootNumber++;
       }
 
-      if (rootCount > 1) {
+      if (rootNumber > 1)
+      {
         return false;
       }
 
-      if (arr[j][i] == 1) {
-        oneCountinColumn++;
+      if (arr[j][i] == 1)
+      {
+        numberOfUnitsInColumn++;
       }
 
-      if (oneCountinColumn >= 2) {
+      if (numberOfUnitsInColumn >= 2)
+      {
         return false;
       }
     }
   }
 
-  if (rootCount == 0) return false;
+  if (rootNumber == 0)
+    return false;
 
   return true;
 }
 
-int getRootIndex(bool **arr, int size) {
+int getRootIndex(bool **arr, int size)
+{
   int rootIndex = 0;
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < size; i++)
+  {
     int count = 0;
     for (int j = 0; j < size; j++)
-      if (arr[j][i] == 0) {
+      if (arr[j][i] == 0)
+      {
         count++;
       }
 
-    if (count == size) {
+    if (count == size)
+    {
       rootIndex = i;
       return i;
     }
@@ -88,42 +113,50 @@ int getRootIndex(bool **arr, int size) {
   return rootIndex;
 }
 
-int main() {
+int main()
+{
   int size;
   cout << " size: ";
   cin >> size;
   bool **arr = new bool *[size];
   inputMatrix(arr, size);
   int rootIndex;
-  if (checkTree(arr, size)) {
+  if (checkTree(arr, size))
+  {
     rootIndex = getRootIndex(arr, size);
 
-    int arr1[size];
-    arr1[0] = rootIndex;
+    int arrOfTree[size];
+    arrOfTree[0] = rootIndex;
 
-    int a = 1;
+    int arrIndex = 1;
 
-    for (int k = 0; k < size; k++) {
-      int i = arr1[k];
+    for (int k = 0; k < size; k++)
+    {
+      int i = arrOfTree[k];
 
-      for (int j = 0; j < size; j++) {
-        if (arr[i][j] == 1) {
-          arr1[a] = j;
-          a++;
+      for (int j = 0; j < size; j++)
+      {
+        if (arr[i][j] == 1)
+        {
+          arrOfTree[arrIndex] = j;
+          arrIndex++;
         }
       }
     }
 
-    Tree a1;
-    for (int i = 0; i < size; i++) {
-      a1.insertNode(arr1[i]);
+    Tree tree;
+    for (int i = 0; i < size; i++)
+    {
+      tree.insertNode(arrOfTree[i]);
     }
 
-    a1.printDFS();
+    tree.printDFS();
 
     cout << endl;
-  } else {
-    cout << ">>>>>>>> ||||  It's not binary tree  |||||| <<<<<<<<<<<<<<  \n";
+  }
+  else
+  {
+    cout << " It's not binary tree \n";
   }
   return 0;
 }
