be5a4251cab0a922aaffc8e30cab8fea1853bc8d
naneohanjanyan <100269250+naneohanjanyan@users.noreply.github.com>
2022-05-05 12:26:47 +0400

    Update createTree.cpp

--- date_structure/day6/createTree.cpp
+++ date_structure/day6/createTree.cpp
@@ -1,78 +1,128 @@
 #include <iostream>
-#include "../day5/node.hpp"
+
 #include "../day4/BinaryTree.hpp"
+#include "../day5/node.hpp"
 using namespace std;
 
-int getRootIndex(bool **arr, int size)
-{
-    int rootIndex = -1;
+bool checkRoot(bool **arr, int size);
 
+void inputMatrix(bool **arr, int size) {
+  for (int i = 0; i < size; i++) {
+    arr[i] = new bool[size];
+    for (int j = 0; j < size; j++) {
+      cin >> arr[i][j];
+    }
+  }
+}
+
+bool checkTree(bool **arr, int size) {
+  for (int i = 0; i < size; i++) {
+    int oneCount = 0;
+    for (int j = 0; j < size; j++) {
+      if (arr[i][j] == 1 && arr[j][i] == 1) {
+        return false;
+      }
+      if (arr[i][j] == 1) {
+        oneCount++;
+      }
+      if (oneCount > 2) {
+        return false;
+      }
+    }
+  }
+  if (!checkRoot(arr, size)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+bool checkRoot(bool **arr, int size) {
+  int rootCount = 0;
+  for (int i = 0; i < size; i++) {
+    int oneCountinColumn = 0;
+    int count = 0;
+    for (int j = 0; j < size; j++) {
+      if (arr[j][i] == 0) {
+        count++;
+      }
+
+      if (count == size) {
+        rootCount++;
+      }
+
+      if (rootCount > 1) {
+        return false;
+      }
+
+      if (arr[j][i] == 1) {
+        oneCountinColumn++;
+      }
+
+      if (oneCountinColumn >= 2) {
+        return false;
+      }
+    }
+  }
+
+  if (rootCount == 0) return false;
+
+  return true;
+}
+
+int getRootIndex(bool **arr, int size) {
+  int rootIndex = 0;
+  for (int i = 0; i < size; i++) {
+    int count = 0;
     for (int j = 0; j < size; j++)
-    {
-        int count = 0;
-        for (int i = 0; i < size; i++)
-            if (arr[i][j] == 0)
-            {
-                count++;
-            }
+      if (arr[j][i] == 0) {
+        count++;
+      }
 
-        if (count == size)
-        {
-            rootIndex = j;
-            break;
-        }
+    if (count == size) {
+      rootIndex = i;
+      return i;
     }
-    return rootIndex;
+  }
+
+  return rootIndex;
 }
 
-void PrintDFS(Node *temp)
-{
-    if (temp == nullptr)
-    {
-        return;
+int main() {
+  int size;
+  cout << " size: ";
+  cin >> size;
+  bool **arr = new bool *[size];
+  inputMatrix(arr, size);
+  int rootIndex;
+  if (checkTree(arr, size)) {
+    rootIndex = getRootIndex(arr, size);
+  }
+
+  int arr1[size];
+  arr1[0] = rootIndex;
+
+  int a = 1;
+
+  for (int k = 0; k < size; k++) {
+    int i = arr1[k];
+
+    for (int j = 0; j < size; j++) {
+      if (arr[i][j] == 1) {
+        arr1[a] = j;
+        a++;
+      }
     }
-    cout << temp->data << "  ";
+  }
 
-    PrintDFS(temp->left);
-    PrintDFS(temp->right);
-}
+  Tree a1;
+  for (int i = 0; i < size; i++) {
+    a1.insertNode(arr1[i]);
+  }
 
-Node *createTree(int index, int size, bool **arr)
-{
-    Node *node = new Node(index);
-    int left = 1;
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[index][i] == 1)
-        {
-            if (left == 1)
-            {
-                node->left = createTree(i, size, arr);
-            }
-            else if (left == 2)
-            {
-                node->right = createTree(i, size, arr);
-            }
-            left++;
-        }
-    }
-    return node;
-}
+  a1.printDFS();
 
-int main()
-{
-    int size = 5;
-    bool **arr = new bool *[size];
-    arr[size][size] = {
-        {0, 0, 0, 1, 1},
-        {0, 0, 0, 0, 0},
-        {1, 1, 0, 0, 0},
-        {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0}};
+  cout << endl;
 
-    int root_index = getRootIndex(arr, size);
-    Node *root = createTree(root_index, size, arr);
-    PrintDFS(root);
-
-    return 0;
+  return 0;
 }
