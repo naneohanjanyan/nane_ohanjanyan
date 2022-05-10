69b363810527a080e69d4a434c0f2334a95d255b
nane <nane.ohanjanyan@mail.ru>
2022-05-11 01:09:39 +0400

    added

---
+++ date_structure/day8/graphAdjacenyList.hpp
@@ -0,0 +1,123 @@
+#ifndef ADJACENY_LIST
+#define ADJACENCY_LIST
+
+#include "list.h"
+#include <iostream>
+
+using namespace std;
+class Graph
+{
+private:
+  List<int> *arr;
+  int _size;
+
+public:
+  Graph(int);
+  Graph();
+  void addNode();
+  void removeNode();
+  void addEdge(int, int);
+  void removeEdge(int, int);
+  void print();
+  void findNeighbrous(int);
+  bool isEdgeExist(int, int);
+};
+
+Graph::Graph(int size)
+{
+  _size = size;
+  List<int> *arr = new List<int>[_size];
+}
+
+Graph::Graph()
+{
+  _size = 0;
+  List<int> *arr = new List<int>[_size];
+}
+
+void Graph::addNode()
+{
+  _size++;
+  List<int> *arr1 = new List<int>[_size];
+
+  for (int i = 0; i < _size - 1; i++)
+  {
+    arr1[i] = arr[i];
+  }
+
+  List<int> *arr2;
+  arr2 = arr;
+  arr = arr1;
+  delete[] arr2;
+}
+
+void Graph::removeNode()
+{
+  _size--;
+  List<int> *arr1 = new List<int>[_size];
+
+  for (int i = 0; i < _size; i++)
+  {
+    if (i != _size)
+    {
+      removeEdge(i, _size);
+    }
+    arr1[i] = arr[i];
+  }
+
+  arr = arr1;
+}
+
+void Graph::addEdge(int node1, int node2)
+{
+  arr[node1].addEnd(node2);
+  arr[node2].addEnd(node1);
+}
+
+void Graph::removeEdge(int node1, int node2)
+{
+  for (int i = 0; i < arr[node1].getSize(); i++)
+  {
+    if (arr[node1].getElement(i) == node2)
+    {
+      arr[node1].removeAt(i);
+      break;
+    }
+  }
+
+  for (int i = 0; i < arr[node2].getSize(); i++)
+  {
+    if (arr[node2].getElement(i) == node1)
+    {
+      arr[node2].removeAt(i);
+      break;
+    }
+  }
+}
+
+void Graph::print()
+{
+  for (int i = 0; i < _size; i++)
+  {
+    cout << i << "-> ";
+    arr[i]._print();
+    cout << endl;
+  }
+}
+
+bool Graph::isEdgeExist(int node1, int node2)
+{
+  if (arr[node1].getNode(node2))
+  {
+    return true;
+  }
+  return false;
+}
+
+void Graph::findNeighbrous(int node)
+{
+  cout << node << " -> ";
+  arr[node]._print();
+}
+
+#endif
---
+++ date_structure/day8/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "graphAdjacenyList.hpp"
+#include "list.h"
+
+using namespace std;
+
+int main()
+{
+  Graph a1;
+  cout << " add node\n";
+  ;
+  a1.addNode();
+  a1.addNode();
+  a1.addNode();
+  a1.addNode();
+
+  a1.print();
+
+  cout << "\n add edge\n";
+
+  a1.addEdge(0, 1);
+  a1.addEdge(0, 2);
+  a1.addEdge(0, 3);
+  a1.addEdge(2, 3);
+  a1.addEdge(1, 2);
+  a1.print();
+
+ //cout << "\nedge exist: " << a1.isEdgeExist(0, 1);
+
+  cout << "\n\n find neighbrous\n";
+  a1.findNeighbrous(0);
+
+  cout << "\n\n remove edge \n";
+  a1.removeEdge(0, 2);
+  a1.print();
+
+  cout << " \n\n remove node  \n";
+  a1.removeNode();
+  a1.print();
+  cout << endl;
+
+  return 0;
+}
