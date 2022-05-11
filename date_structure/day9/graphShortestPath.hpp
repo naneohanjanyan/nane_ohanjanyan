c1dda9948265c4a277e86ebfe79ce398031284c7
nane <nane.ohanjanyan@mail.ru>
2022-05-11 23:40:19 +0400

    graph dijkstra impliment

--- date_structure/day9/graphShortestPath.hpp
+++ date_structure/day9/graphShortestPath.hpp
@@ -1,66 +1,87 @@
 #ifndef PATH_H
 #define PATH_H
 
+#include <iostream>
 #include <climits>
 using namespace std;
 
-class Graph {
- private:
-  int size;
-  int **arr;
+class Graph
+{
+private:
+  int _size;
+  int **_arr;
+  int findMinDistance(int distace[], bool isVisited[]);
 
- public:
+public:
   Graph() = default;
-  Graph(int **_arr, int _size);
-  //   ~Graph();
-  void findDistance();
-  void inputMatrix(int **, int);
-  void print();
+  Graph(int **arr, int size);
+  ~Graph();
+  void findDistance(int startVertex);
 };
 
-Graph::Graph(int **_arr, int _size) {
-  size = _size;
-  arr = _arr;
+Graph::Graph(int **arr, int size)
+{
+  _size = size;
+  _arr = arr;
 }
 
-void Graph::inputMatrix(int **arr, int size) {
-  for (int i = 0; i < size; i++) {
-    arr[i] = new int[size];
-    for (int j = 0; j < size; j++) {
-      cin >> arr[i][j];
-      if (arr[i][j] = -1) {
-        arr[i][j] = INT_MAX;
+Graph::~Graph()
+{
+  delete[] _arr;
+}
+
+int Graph::findMinDistance(int distace[], bool isVisited[])
+{
+  int min = INT_MAX;
+  int index;
+  for (int i = 0; i < _size; i++)
+  {
+    if (!isVisited[i] && distace[i] <= min)
+    {
+      min = distace[i];
+      index = i;
+    }
+  }
+  return index;
+}
+
+void Graph::findDistance(int startVertex)
+{
+  bool isvisited[_size];
+  int distance[_size];
+
+  for (int i = 0; i < _size; i++)
+  {
+    distance[i] = INT_MAX;
+    isvisited[i] = false;
+  }
+
+  distance[startVertex] = 0;
+
+  for (int i = 0; i < _size; i++)
+  {
+    int minWeightIndex = findMinDistance(distance, isvisited);
+    isvisited[startVertex] = true;
+
+    if (distance[minWeightIndex] == INT_MAX)
+    {
+      break;
+    }
+
+    for (int j = 0; j < _size; j++)
+    {
+      if (_arr[minWeightIndex][j] != INT_MAX && distance[minWeightIndex] != INT_MAX && distance[minWeightIndex] + _arr[minWeightIndex][j] < distance[j])
+      {
+        distance[j] = distance[minWeightIndex] + _arr[minWeightIndex][j];
       }
     }
+    isvisited[minWeightIndex] = true;
+  }
+
+  for (int i = 0; i < _size; i++)
+  {
+    cout << distance[i] << "  ";
   }
 }
-
-void Graph::print() {
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      cout << arr[i][j] << "  ";
-    }
-    cout << endl;
-  }
-}
-
-void Graph ::findDistance(int start_vertex) {
-  bool is_visited[size] = {false};
-  int distance[size] = {INT_MAX};
-
-  distance[start_vertex] = 0;
-  is_visited[start_vertex] = true;
-
-  for (int i = 0; i < size; i++) {
-    if (arr[start_vertex][i] != INT_MAX && arr[start_vertex][i] != 0) {
-      distance[i] = arr[start_vertex][i];
-    }
-  }
-
-  //   for (int i = 0; i < size; i++) {
-  //     for (int j = 0; i < size; i++) {
-  //     }
-  //   }
-  // }
 
 #endif
