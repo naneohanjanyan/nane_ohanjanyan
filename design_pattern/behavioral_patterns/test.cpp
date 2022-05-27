684ab3318915d2485b98d0f6c3de4f66940481ed
nane <nane@mail.ru>
2022-05-28 02:11:28 +0400

    test commit

---
+++ design_pattern/behavioral_patterns/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+  for (int i = 0; i < 5; i++)
+  {
+    cout << i << endl;
+  }
+  double name;
+  int ananaa;
+  if (true)
+  {
+    int a = 0;
+  }
+  ananaa = 2;
+  return 0;
+}
