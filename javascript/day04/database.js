7f446e25150fe0de1ff9953b59576d01de37f2f4
nane <nane@mail.ru>
2022-06-21 23:24:30 +0400

    add

---
+++ javascript/day04/database.js
@@ -0,0 +1,51 @@
+class Database {
+  static id = 1;
+  static instance = null;
+
+  static get() {
+    if (localStorage.getItem("db") === null) {
+      localStorage.setItem("db", JSON.stringify([], undefined, 2));
+    }
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+
+  getTable(name) {
+    const arr = JSON.parse(localStorage.getItem("db"));
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].name === `${name}`) return new Table(name);
+    }
+  }
+
+  createTable(name, column, values = null) {
+    let arr = [];
+    arr = JSON.parse(localStorage.getItem("db"));
+
+    if (!arr.find((element) => element.name === name)) {
+      arr.push({
+        name: `${name}`,
+        columns: ['id', ...column],
+        values: [Database.id, ...values],
+      });
+    }
+    localStorage.setItem("db", JSON.stringify(arr));
+    Database.id++;
+  }
+}
+
+class Table {
+  columns;
+  values;
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+let temp = Database.get();
+temp.createTable("user", ["username", "password"], ["Nane", "asd"]);
+temp.createTable("admin", ["username", "password", 'age'], ["Gagik", "xyz", 25]);
+
+console.log(temp.getTable("admin"));
+console.log(temp.getTable("user"));
