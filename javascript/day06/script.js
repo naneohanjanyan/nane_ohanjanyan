a069ff9c8198c4631cddbff742e720a13e5494b8
nane <nane@mail.ru>
2022-06-27 18:37:31 +0400

    update

--- javascript/day06/script.js
+++ javascript/day06/script.js
@@ -3,7 +3,6 @@ if (sessionStorage.getItem('highScoore') !== null) {
 }
 
 let random = Math.floor(Math.random() * 101);
-alert(random);
 let num = document.querySelector("#input");
 let count = document.querySelector("#countValue");
 let score = document.querySelector("#scoreValue");
