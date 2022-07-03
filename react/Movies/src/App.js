2c2b9ef80160f6cf0cf7284a2b59c20de0273fc3
nane <nane@mail.ru>
2022-07-03 12:44:13 +0400

    update

--- react/Movies/src/App.js
+++ react/Movies/src/App.js
@@ -1,9 +1,9 @@
 import { changedArr, RenderMovies } from "./renderMoves";
 import { useEffect, useState, useRef } from "react";
-import moviesContainer from "./MoviesContainer";
+import Button from "./Button/Button";
 import Modal from "./Modal/Modal";
+import moviesContainer from "./MoviesContainer";
 import Spider from "./images/download.jpeg";
-import { Button } from "./Button/Button";
 
 function moviesFilter(name) {
   const filteredАrr =
@@ -19,11 +19,11 @@ function moviesFilter(name) {
 
 export function App() {
   const [count, setCount] = useState(moviesContainer.length);
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [newArr, setNewArr] = useState([]);
   const [show, setShow] = useState(false);
-  const [newFilmName, setNewFilmName] = useState("");
-  const [newFilmDesc, setNewFilmDesc] = useState("");
+  const [newFilmName, setNewFilmName] = useState('');
+  const [newFilmDesc, setNewFilmDesc] = useState('');
   const refNewFilmNameInputClear = useRef();
   const refNewFilmDescInputClear = useRef();
 
@@ -58,26 +58,20 @@ export function App() {
             setTimeout(() => setName(event.target.value), 500);
           }}
         ></input>
-        <Button onClick={() => setShow(true)}>add</Button> {/* button component */}
-
+        <Button onClick={() => setShow(true)}>add</Button>
         <Modal title="My Modal" onClose={() => setShow(false)} show={show}>
-
-          <p>Add filme
-            <Button onClick={() => setShow(false)}>X</Button>  {/* button component */}
+          <p>Add film
+            <Button onClick={() => setShow(false)}>X</Button>
           </p>
-
           <div className="modalContent">
             <input type="text" placeholder="Film name" ref={refNewFilmNameInputClear} className="filmName style__"
               onChange={(event) => setNewFilmName(event.target.value)} />
-
             <input type="text" placeholder="description" ref={refNewFilmDescInputClear} className="description style__"
               onChange={(event) => setNewFilmDesc(event.target.value)} />
-            <Button onClick={addMovie} >Add</Button>
+            <Button onClick={addMovie}>Add</Button>
           </div>
-
         </Modal>
       </div>
-
       <div id="films">
         <RenderMovies newArr={newArr} setArr={setNewArr} setCount={setCount} />
       </div>
--- react/Movies/src/Button/Button.js
+++ react/Movies/src/Button/Button.js
@@ -1,8 +1,9 @@
 import "./Button.css"
 
 export const Button = ({ onClick, children }) => {
-    console.log("remove")
     return (
         <button className="buttonComponent" onClick={onClick}>{children}</button>
     );
 }
+
+export default Button;
